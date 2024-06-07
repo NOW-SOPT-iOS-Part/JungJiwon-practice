b407c72647bb28b019560a43fbdd94e36a31c672
codeJiwon <loopy0107@sookmyung.ac.kr>
2024-06-07 17:13:26 +0900

    Feat: 7주차 실습

---
+++ week7/week7/ViewModelType.swift
@@ -0,0 +1,15 @@
+//
+//  ViewModelType.swift
+//  week7
+//
+//  Created by  정지원 on 5/25/24.
+//
+
+import RxSwift
+
+protocol ViewModelType {
+    associatedtype Input
+    associatedtype Output
+    
+    func transform(from input: Input, disposeBag: RxSwift.DisposeBag) -> Output
+}
